2c47a9b7af01b923d1177221a3672ffc0c034f30
Mary Rose <mary.rose@acquia.com>
2022-10-10 19:28:34 -0700

    add notes to test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,19 +3,24 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  //Compiles the components
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
+      //Declares the component to be tested
       declarations: [
         AppComponent
       ],
+    //Compiles component’s resources like the template, styles etc.  
     }).compileComponents();
   });
 
   it('should create the app', () => {
+    //fixture is a way of fixing the state of a component and modelling it in a browser environment
     const fixture = TestBed.createComponent(AppComponent);
+    //componentInstance is the instance of the root component class
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
